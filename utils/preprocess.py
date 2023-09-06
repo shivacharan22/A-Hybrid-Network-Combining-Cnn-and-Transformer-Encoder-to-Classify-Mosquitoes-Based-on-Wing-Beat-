85fc66fb0d11e5e5fc6fde2acc70d7cf5db2746a
Oruganti Shiva Charan <54499416+shivacharan22@users.noreply.github.com>
2023-09-06 01:34:28 -0400

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -6,10 +6,18 @@ import pandas as pd
 
 
 def transform_inpu(input : Union[str, pd.DataFrame]) -> Tensor:
-    """ transform input to 8000Hz used by pytorch dataset 
-        Args:
-            input: the path of the audio file or the dataframe
-            Returns: transformed Tensor  
+    """
+    Transform the input audio data to have a sample rate of 8000Hz for use in PyTorch datasets.
+
+    Args:
+        input (Union[str, pd.DataFrame]): The path to the audio file or a pandas DataFrame containing audio data.
+
+    Returns:
+        Tensor: Transformed audio data with a sample rate of 8000Hz.
+
+    Example:
+        waveform = transform_inpu('path/to/audio.wav')  # Transform audio file.
+        transformed_data = transform_inpu(data_frame)  # Transform audio data in a DataFrame.
     """
     waveform2, sample_rate2 = torchaudio.load(input)
     if sample_rate2 != 8000:
