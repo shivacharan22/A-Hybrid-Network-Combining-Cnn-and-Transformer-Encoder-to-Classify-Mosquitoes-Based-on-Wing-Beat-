d3b66f4228f8c2f82507ee14419aa1a419973a75
Oruganti Shiva Charan <54499416+shivacharan22@users.noreply.github.com>
2023-09-06 01:22:50 -0400

    Update custom_dataset.py

--- utils/custom_dataset.py
+++ utils/custom_dataset.py
@@ -5,7 +5,27 @@ from utils.preprocess import transform_inpu
 
 class CustomImageDataset(Dataset):
 
-    """ Custom dataset Class for the mosquito wingbeat dataset"""
+    """
+    Custom dataset class for the mosquito wingbeat dataset.
+
+    Args:
+        annotations_file (pd.DataFrame): A Pandas DataFrame containing dataset annotations.
+
+    Attributes:
+        data (pd.DataFrame): The DataFrame containing dataset annotations.
+        lab_dict (dict): A dictionary mapping class labels to numerical indices.
+
+    Methods:
+        __len__(): Returns the total number of samples in the dataset.
+        __getitem__(idx): Returns a specific sample from the dataset.
+
+    Returns:
+        tuple: A tuple containing the transformed input data and its corresponding label.
+
+    Example:
+        dataset = CustomImageDataset(annotations_file)
+        sample = dataset[0]
+    """
 
     def __init__(self, annotations_file):
         self.data = annotations_file
